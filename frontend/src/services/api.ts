371d2574050f02d8bb2f9f91d68d08036a65889a
MateusTC <privado.mtc@gmail.com>
2026-04-09 07:59:11 -0300

    09-04-V1

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,23 +1,7 @@
-import axios from 'axios';
+import { createClient } from '@supabase/supabase-js';
 
-// Criamos uma instância do Axios para não ter que repetir a URL em todas as telas
-const api = axios.create({
-  // baseURL: É o endereço principal do servidor
-  // Todas as requisições que a gente fizer vai usar esse início:
-  baseURL: 'http://enfermapp.great-site.net/backend/public/',
+// Conexão direta com o SUPABASE
+const supabaseUrl = 'https://avmupljrhfnbklsortti.supabase.co';
+const supabaseKey = 'sb_publishable_95sIeMO0b43Pn_37vtlJew_Zaglz3fa';
 
-  // timeout: Define quanto tempo (em milissegundos) o app deve esperar 
-  // o servidor responder antes de desistir e dar erro. (10 segundos aqui)
-  timeout: 10000,
-
-  // headers: São as "etiquetas" da requisição
-  headers: {
-    // Content-Type: Avisa ao servidor PHP que estamos enviando dados em formato JSON.
-    'Content-Type': 'application/json',
-    // Accept: Avisa ao servidor que o celular espera receber um JSON como resposta.
-    'Accept': 'application/json',
-  }
-});
-
-// Exportamos a instância para que as telas (como a index.tsx) possam importá-la
-export default api;
+export const supabase = createClient(supabaseUrl, supabaseKey);
